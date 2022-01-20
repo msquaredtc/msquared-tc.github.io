70a4eaff61b6f5b5d6089403027485a96a5caf22
xuxo garcia <j.maria.garcia@outlook.com>
2022-01-20 10:53:19 -0700

    Adding video

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -30,7 +30,9 @@ jQuery(document).ready(function() {
         Background slideshow
     */
 	/*$('.top-content').backstretch("assets/img/backgrounds/blur-close-up-color-dark-1129253.jpg");*/
-	$('.top-content').backstretch("assets/img/backgrounds/bigbanner.svg");
+	//$('.top-content').backstretch("assets/img/backgrounds/bigbanner.svg");
+	$('.top-content').backstretch([ { url: "assets/video/pexels-ivan-samkov-9640795.mp4",
+									 mute: true, isVideo: true, loop: true }]);
     $('.call-to-action-container').backstretch("assets/img/backgrounds/1.jpg");
 	$('.testimonials-container').backstretch("assets/img/backgrounds/1.jpg");
 	//$('.about-us-container').backstretch("assets/img/about/miami.jpg");
